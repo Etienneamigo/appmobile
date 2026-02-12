83df86ccd0e730d36c280b31a6df59a7e1f6cfb8
Etienne Amigo <etienne.amigo@icloud.com>
2026-02-12 23:18:09 +0100

    chore: mobile folder only (from claude/fix-mobile-ux-bugs-zyJ5P)

--- prisma/migrations/20260209120000_add_zone_tags/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Activity" ADD COLUMN "zone1Tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
-ALTER TABLE "Activity" ADD COLUMN "zone2Tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
-ALTER TABLE "Activity" ADD COLUMN "zone3Tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
